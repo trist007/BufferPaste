0867dd8f19409022669d7b97529e970c7bad7013
Tristan <trist007@darkterminal.net>
2025-06-07 11:14:20 -0500

    renamed from copypasta to bufferpaste

--- bufferpaste/code/build.bat
+++ bufferpaste/code/build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set CommonCompilerFlags=-MT -nologo -Gm- -GR- -EHa- -Od -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -DCOPYPASTA_INTERNAL=1 -DCOPYPASTA_SLOW=1 -DCOPYPASTA_WIN32=1 -FC -Z7
+set CommonCompilerFlags=-MT -nologo -Gm- -GR- -EHa- -Od -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -DBUFFERPASTE_INTERNAL=1 -DBUFFERPASTE_SLOW=1 -DBUFFERPASTE_WIN32=1 -FC -Z7
 set CommonLinkerFlags= -incremental:no -opt:ref user32.lib gdi32.lib
 
 REM TODO - can we just build both with one exe?
@@ -13,5 +13,5 @@ REM cl %CommonCompilerFlags% ..\handmade\code\win32_handmade.cpp /link -subsyste
 
 REM 64-bit build
 REM del *.pdb > NUL 2> NUL
-cl %CommonCompilerFlags% ..\copypasta\code\win32_copypasta.cpp /link %CommonLinkerFlags%
+cl %CommonCompilerFlags% ..\bufferpaste\code\win32_bufferpaste.cpp /link %CommonLinkerFlags%
 popd
--- bufferpaste/misc/shell.bat
+++ bufferpaste/misc/shell.bat
@@ -6,7 +6,7 @@ REM  %windir%\system32\cmd.exe /k w:\handmade\misc\shell.bat
 REM
 
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-set path=c:\users\thart\dev\CopyPasta;%path%
+set path=c:\users\thart\dev\BufferPaste;%path%
 doskey gitlogd=git log --pretty=oneline
 doskey vi=nvim
-REM start /B "" c:\remedybg\remedybg.exe c:\users\thart\dev\CopyPasta\build\win32_copypasta.exe
+REM start /B "" c:\remedybg\remedybg.exe c:\users\thart\dev\BufferPaste\build\win32_bufferpaste.exe
